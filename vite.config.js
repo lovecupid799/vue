8ace01b5687c8ab0dfe34f0b5dddbebf574a74e9
ohy <lovecupid799@gmail.com>
2025-05-18 21:35:58 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-mob/',
+  base:  '/vue-mob/' ,
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
