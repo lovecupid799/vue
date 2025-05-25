9f60652181900abe8b3a658e2e7cc8439bd02d68
ohy <lovecupid799@gmail.com>
2025-05-25 22:06:54 +0900

    update1

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WorkSheet from '../views/guide/WorkSheet.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,14 +7,6 @@ const router = createRouter({
       path: '/',
       name: 'WorkSheet',
       component: () => import('../views/guide/WorkSheet.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
     },
   ],
 })
