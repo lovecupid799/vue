e1441d22d0b9dbf8b88c6cb78e0f820720f56f6a
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-06-04 11:14:47 +0900

    Update GuideForm.vue

--- src/views/guide/GuideForm.vue
+++ src/views/guide/GuideForm.vue
@@ -2,27 +2,33 @@
   <h2 class="guide-tit1">FORM</h2>
   <h3 class="guide-tit2">input</h3>
   <h4 class="guide-tit3">타입 - 기본</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 
   <h3 class="guide-tit2">라디오</h3>
   <h4 class="guide-tit3">타입 - 기본</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+  <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 
   <h4 class="guide-tit3">라디오 그룹</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 </template>
