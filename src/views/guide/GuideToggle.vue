7d184faf4485ae3a0ee1bc08d4e462d17c06857e
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-06-04 11:15:33 +0900

    Update GuideToggle.vue

--- src/views/guide/GuideToggle.vue
+++ src/views/guide/GuideToggle.vue
@@ -2,16 +2,23 @@
   <h2 class="guide-tit1"> COLLAPSE</h2>
   <h3 class="guide-tit2">토글</h3>
   <h4 class="guide-tit3">타입 - 기본</h4>
-  <div class="guide-txtBox">
-    <pub-toggle />
+   <div class="guide-exBox">
+    <div class="in">
+      <pub-toggle />
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 
   <h3 class="guide-tit2">아코디언</h3>
   <h4 class="guide-tit3">타입 - 기본</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 </template>
