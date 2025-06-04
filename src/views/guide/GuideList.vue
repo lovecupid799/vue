e38860f4ae414c12ec94cf2408975b11ef8cf77f
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-06-04 11:15:02 +0900

    Update GuideList.vue

--- src/views/guide/GuideList.vue
+++ src/views/guide/GuideList.vue
@@ -2,18 +2,22 @@
   <h2 class="guide-tit1">LIST</h2>
   <h3 class="guide-tit2">list</h3>
   <h4 class="guide-tit3">타입 - 기본</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 
   <h4 class="guide-tit3">타입 - 블릿</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 </template>
