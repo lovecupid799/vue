bb99c841b0c5c9fed65378d08437e3846ad24161
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-06-04 13:29:12 +0900

    Update pubButtonWrap.vue

--- src/components/control/button/pubButtonWrap.vue
+++ src/components/control/button/pubButtonWrap.vue
@@ -1,10 +1,62 @@
 <template>
-  <div class="cnt-button">
-    <slot></slot>
+  <div
+    class="cnt-btn-area"
+    :class="[dataPlacement !== null && (dataPlacement === 'top' ? 'top' : 'btm'), { center: dataAlign === 'center' }, dataType === null ? 'type-cnt' : `cnt-btm  type-${dataType}`]">
+    <div
+      class="left"
+      v-if="!!$slots.left">
+      <slot name="left"></slot>
+    </div>
+
+    <div
+      class="right"
+      v-if="dataAlign === null">
+      <slot></slot>
+    </div>
+    <template v-if="dataAlign === 'center'">
+      <slot></slot>
+    </template>
   </div>
 </template>
-
-<script setup>
+<script>
+export default {
+  name: 'NspBtnArea',
+  props: {
+    dataPlacement: {
+      type: [String, null],
+      default: null,
+    },
+    dataAlign: {
+      type: [String, null],
+      default: null,
+    },
+    dataType: {
+      type: [String, null],
+      default: null,
+    },
+  },
+};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.cnt-btn-area {
+  &.center {
+    justify-content: center;
+    gap: 20px;
+  }
+  &.btm {
+    margin-top: 24px;
+  }
+  &.type-cnt {
+    gap: 8px;
+  }
+  &.type-page {
+    &.top {
+      margin-bottom: 32px;
+    }
+    &.btm {
+      margin-top: 32px;
+    }
+  }
+}
+</style>
