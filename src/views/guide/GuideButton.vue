b5e878662ef23aa5339dc68e004079149153675e
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-06-04 11:13:56 +0900

    Update GuideButton.vue

--- src/views/guide/GuideButton.vue
+++ src/views/guide/GuideButton.vue
@@ -2,27 +2,33 @@
   <h2 class="guide-tit1">BUTTON</h2>
   <h3 class="guide-tit2">요소</h3>
   <h4 class="guide-tit3">타입 - 기본</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+  <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 
   <h4 class="guide-tit3">타입 - 라운드</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 
   <h3 class="guide-tit2">버튼 영역</h3>
   <h4 class="guide-tit3">타입 - 페이지 버튼 영역</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 </template>
