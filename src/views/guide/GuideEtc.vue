fb4e6253a76985c1ed15071b415840155de15a1f
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-06-04 11:14:26 +0900

    Update GuideEtc.vue

--- src/views/guide/GuideEtc.vue
+++ src/views/guide/GuideEtc.vue
@@ -2,24 +2,33 @@
   <h2 class="guide-tit1">ETC</h2>
   <h3 class="guide-tit2">Badge</h3>
   <h4 class="guide-tit3">타입 - 기본</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 
   <h4 class="guide-tit3">타입 - 블릿</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 
   <h3 class="guide-tit2">CHIPS</h3>
   <h4 class="guide-tit3">타입 - 기본</h4>
-  <div class="guide-txtBox">
-    <pub-chips />
+   <div class="guide-exBox">
+    <div class="in">
+       <pub-chips />
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 </template>
