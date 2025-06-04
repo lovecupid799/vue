27f82a30dc29244df75b0dfca219dd873c119409
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-06-04 11:15:52 +0900

    Update GuideText.vue

--- src/views/guide/GuideText.vue
+++ src/views/guide/GuideText.vue
@@ -2,34 +2,43 @@
   <h2 class="guide-tit1">TYPOGRAPHY</h2>
   <h3 class="guide-tit2">Title</h3>
   <h4 class="guide-tit3">뎁스</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 
   <h3 class="guide-tit2">Text</h3>
   <h4 class="guide-tit3">보조텍스트</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+  <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 
   <h4 class="guide-tit3">바디 텍스트</h4>
-  <div class="guide-txtBox">
-    <ul class="ul">
-      <li></li>
-      <li></li>
-    </ul>
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
+    </div>
   </div>
 
   <h3 class="guide-tit2">타이틀 영역</h3>
-  <div class="guide-box ">
-    <div class="guide-in">
-      <pub-title />
+   <div class="guide-exBox">
+    <div class="in">
+      
+    </div>
+    <div class="code">
+      <pre>{{ ``}}</pre>
     </div>
   </div>
 </template>
