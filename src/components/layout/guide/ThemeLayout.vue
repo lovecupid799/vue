2265e3d0131a068096074f630980e42c0994da20
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-06-10 15:57:16 +0900

    Create ThemeLayout.vue

---
+++ src/components/layout/guide/ThemeLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    id="wrap"
+    class="main layout-3101 type1"
+    data-theme="3101">
+    <pub-header />
+    <div id="container">
+      <router-view />
+    </div>
+    <pub-footer />
+  </div>
+</template>
