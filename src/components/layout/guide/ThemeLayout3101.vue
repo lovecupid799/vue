7bdf16518610cbe783009cd32e03e5f4bd1ed74b
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-06-10 15:56:45 +0900

    Create ThemeLayout3101.vue

---
+++ src/components/layout/guide/ThemeLayout3101.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    id="wrap"
+    class="main layout-3101 type2"
+    data-theme="3101">
+    <pub-header />
+    <div id="container">
+      <router-view />
+    </div>
+    <pub-footer />
+  </div>
+</template>
+
+<script setup>
+import PubHeader from '@/components-pub/common/layouts/com/PubHeader3101.vue'; // pub-header
+</script>
