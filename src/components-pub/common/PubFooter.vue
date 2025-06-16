096bf1b6c69442b8b32b1f2e98ceb59c91b47039
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-06-16 14:49:26 +0900

    Update PubFooter.vue

--- src/components-pub/common/PubFooter.vue
+++ src/components-pub/common/PubFooter.vue
@@ -1,6 +1,19 @@
 <template>
-    <div id="footer">
-        Footer
+  <footer id="footer">
+    <div class="inner">
+      <div class="f-left">
+        <p class="f-copy">© Samsung</p>
+      </div>
+      <div class="f-right">
+        <div class="f-menu">
+          <router-link>Training</router-link>
+          <router-link>Privacy Policy</router-link>
+          <router-link>Legal</router-link>
+          <router-link>FAQ</router-link>
+          <router-link>Q&A</router-link>
+          <router-link>Contact Us</router-link>
+        </div>
+      </div>
     </div>
+  </footer>
 </template>
-
