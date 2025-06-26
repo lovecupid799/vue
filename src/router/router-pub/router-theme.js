c4fef1af87c7c0a3eaecdb9caef86b05d921ab60
ohy <lovecupid799@gmail.com>
2025-06-26 20:59:20 +0900

    update

--- src/router/router-pub/router-theme.js
+++ src/router/router-pub/router-theme.js
@@ -10,7 +10,7 @@ const themeRoutes = [
     component: () => import('@/components-pub/layouts/ThemeLayout.vue'),
     children: [
       {
-        path: '/theme1/theme',
+        path: '/theme1',
         name: 'theme1',
         component: () => import('@/views/_PubSample/SampleTheme.vue'),
       },
@@ -21,7 +21,7 @@ const themeRoutes = [
     component: () => import('@/components-pub/layouts/ThemeLayout3101.vue'),
     children: [
       {
-        path: '/theme2/theme2',
+        path: '/theme2',
         name: 'theme2',
         component: () => import('@/views/_PubSample/SampleTheme.vue'),
       },
