fb6a4c740e8e6bff29b6f76d3b90c95f91bfb294
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-06-26 10:52:20 +0900

    Update router-theme.js

--- src/router/router-pub/router-theme.js
+++ src/router/router-pub/router-theme.js
@@ -6,22 +6,22 @@
 
 const themeRoutes = [
   {
-    path: 'theme1',
+    path: '/theme1',
     component: () => import('@/components-pub/layouts/ThemeLayout.vue'),
     children: [
       {
-        path: '/theme/SampleTheme1',
+        path: '/theme1/SampleTheme1',
         name: 'SampleTheme1',
         component: () => import('@/views/_PubSample/SampleTheme.vue'),
       },
     ],
   },
   {
-    path: 'theme2',
+    path: '/theme2',
     component: () => import('@/components-pub/layouts/ThemeLayout3101.vue'),
     children: [
       {
-        path: '/theme/SampleTheme2',
+        path: '/theme2/SampleTheme2',
         name: 'SampleTheme2',
         component: () => import('@/views/_PubSample/SampleTheme.vue'),
       },
