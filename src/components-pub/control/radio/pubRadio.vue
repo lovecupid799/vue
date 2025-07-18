1924bed9ed5ae2ee735d079934d9cfc997f2a0c7
lovecupid799 <93204616+lovecupid799@users.noreply.github.com>
2025-07-18 14:00:48 +0900

    Update pubRadio.vue

--- src/components-pub/control/radio/pubRadio.vue
+++ src/components-pub/control/radio/pubRadio.vue
@@ -1,70 +1,92 @@
 <template>
-  <div :class="setFormClass">
+  <div class="form-check-input" :class="setFormClass">
     <input
-      class="form-radio-input"
+      class="form-radio"
       type="radio"
       :id="id"
       :value="value"
       :disabled="disabled"
-      v-model="checked"
-      @change="updateCheckValue"
+      :checked="isChecked"
+      @change="$emit('update:modelValue', $event.target.value)"
     />
-  <label :class="labelClass" :for="id"><slot></slot></label>
+    <label :class="labelClass" :for="id"><slot></slot></label>
   </div>
 </template>
 
 <script setup>
 import { uniqueId } from 'lodash'
-import { ref, computed, defineProps, defineEmits, onMounted, watch } from 'vue'
+import { ref, computed, defineProps } from 'vue'
 
 const props = defineProps({
-    inline: Boolean,
-    modelValue: [String, Object],
-    value: String,
-    disabled: Boolean,
-    labelClass: { type: String, default: 'form-check-label' },
-});
+  inline: Boolean,
+  modelValue: [String, Object],
+  value: String,
+  disabled: Boolean,
+  labelClass: { type: String, default: 'form-check-label' },
+})
 
-const formClass = ref('form-check');
-const id = uniqueId('sdl-check-box');
-const checked = ref('');
-const checkList = ref([]);
+const formClass = ref('form-check')
+const id = uniqueId('sdl-check-box')
 
-const setFormClass = computed(() =>{
+const setFormClass = computed(() => {
   if (props.inline) return formClass.value + ' form-check-inline'
   return props.formClass
-});
+})
 
-const emit = defineEmits(['update:modelValue']);
-const updateCheckValue = (event) => {
-  if ('object' === typeof props.modelValue) {
-    checkList.value = props.modelValue
-    if (checked.value) {
-      checkList.value.push(event.target.value)
-    } else {
-      checkList.value.splice(checkList.value.indexOf(event.target.value), 1)
-    }
-    emit('update:modelValue', checkList.value)
-  } else {
-    emit('update:modelValue', checked.value ? event.target.value : '')
-  }
-};
-
-const syncValue = () =>{
-  if ('object' === typeof props.modelValue) {
-    if (props.modelValue.includes(props.value)) checked.value = true
-    else checked.value = false
-  } else {
-    if (props.modelValue === props.value) checked.value = true
-    else checked.value = false
-  }
-};
-
-watch(() => props.modelValue,  (newValue, oldValue) => {
-    syncValue();
-});
-
-onMounted(()=>{
-  syncValue()
-});
+const isChecked = computed(() => {
+  return props.modelValue === props.value
+})
 </script>
+
+<style lang="scss">
+.form-check-input {
+  position: relative;
+  input[type='radio'] {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
+  .form-check-label {
+    display: flex;
+    position: relative;
+    &::before {
+      display: inline-flex;
+      content: '';
+      width: 16px;
+      height: 16px;
+      margin: 2px 8px 2px 0;
+    }
+  }
+}
+
+/* ** radio ** */
+input[type='radio'] {
+  ~ .form-check-label {
+    &::before {
+      border-radius: 8px;
+      border: 1px solid #555;
+      background: #fff;
+    }
+  }
+  &:checked {
+    ~ .form-check-label {
+      &::before {
+        border-color: #0b219e;
+      }
+      &::after {
+        position: absolute;
+        left: 3px;
+        top: 5px;
+        content: '';
+        width: 10px;
+        height: 10px;
+        border-radius: 50px;
+        background-color: #0b219e;
+      }
+    }
+  }
+}
+</style>
