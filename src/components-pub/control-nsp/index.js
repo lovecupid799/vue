d0f4084beb0be19a49e921f1925d7f54d3e0bc2b
ohy <lovecupid799@gmail.com>
2025-08-17 19:21:54 +0900

    dd

--- src/components-pub/control-nsp/index.js
+++ src/components-pub/control-nsp/index.js
@@ -1,12 +1,12 @@
 import PrdCard from '../control-nsp/NspPrdList/PrdCard.vue'
 import PrdListPage from '../control-nsp/NspPrdList/PrdListPage.vue'
 import NspRadio from '../control-nsp/NspRadio/NspRadio.vue'
-import PrdCardList from '../control-nsp/NspPrdList/PrdCardList.vue'
+// import PrdCardList from '../control-nsp/NspPrdList/PrdCardList.vue'
 
 export default {
   install(Vue) {
     Vue.component('prd-list-page', PrdListPage)
-    Vue.component('prd-card-list', PrdCardList)
+    // Vue.component('prd-card-list', PrdCardList)
     Vue.component('prd-card', PrdCard)
     Vue.component('nsp-radio', NspRadio)
   },
